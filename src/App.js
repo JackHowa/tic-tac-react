b9516316c7a07e7942719d1b8e8989467f3cea65
jackhowa <jackhowaa@gmail.com>
2018-04-11 21:49:22 -0500

    Add new board constructors, scoped onClick functions

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Board from './Board.js';
 class App extends Component {
   render() {
     return (
-			<div>
-				<Board />
-      </div>
+        <div>
+          <Board/>
+        </div>
     );
   }
 }
