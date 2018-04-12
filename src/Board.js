641f57c014f3a28e50d02e46db504f9e59ff034a
jackhowa <jackhowaa@gmail.com>
2018-04-11 21:49:56 -0500

    Add new board constructors, scoped onClick functions

--- src/Board.js
+++ src/Board.js
@@ -1,45 +1,76 @@
 import React, { Component } from 'react';
 import Cell from './Cell.js';
 import CharacterPicker from './CharacterPicker.js';
+import './grid.css';
 
 class Board extends Component {
-	constructor(props) {
+
+  constructor(props) {
     super(props);
     this.state = {
-      cells: Array(9).fill('X'),
+      cells: Array(9).fill('_'),
+      humanCharacter: '',
     };
-	}
+  }
 
-	renderCell(index) {
-		return <Cell index={index} value={this.state.cells[index]} />
-	}
-	
-	render() {
-		return (
-			<div>
-				<CharacterPicker />
-				<table>
-					<tbody>
-						<tr>
-							{this.renderCell(0)}
-							{this.renderCell(1)}
-							{this.renderCell(2)}
-						</tr>
-						<tr>
-							{this.renderCell(3)}
-							{this.renderCell(4)}
-							{this.renderCell(5)}
-						</tr>
-						<tr>
-							{this.renderCell(6)}
-							{this.renderCell(7)}
-							{this.renderCell(8)}
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		)
-	}
+  pickCharacter = (character) => {
+    this.setState({
+      humanCharacter: character,
+    });
+  };
+
+  fillCharacter = (humanCharacter, index) => {
+    if (humanCharacter === '') {
+      alert('Please pick a character -- X or 0');
+      return;
+    }
+    let cells = this.state.cells;
+    cells[index] = humanCharacter;
+    this.setState({
+      cells,
+    });
+  };
+
+  renderCell(index) {
+    return <Cell index={index} value={this.state.cells[index]}
+                 onClickFunction={this.fillCharacter}
+                 humanCharacter={this.state.humanCharacter}/>;
+  }
+
+  render() {
+    return (
+        <div>
+          <div>
+            <CharacterPicker onClickFunction={this.pickCharacter}
+                             character={'O'}/>
+            <CharacterPicker onClickFunction={this.pickCharacter}
+                             character={'X'}/>
+          </div>
+          <div className={'container'}>
+            <table>
+              <tbody>
+              <tr>
+                {this.renderCell(0)}
+                {this.renderCell(1)}
+                {this.renderCell(2)}
+              </tr>
+              <tr>
+                {this.renderCell(3)}
+                {this.renderCell(4)}
+                {this.renderCell(5)}
+              </tr>
+              <tr>
+                {this.renderCell(6)}
+                {this.renderCell(7)}
+                {this.renderCell(8)}
+              </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+    )
+        ;
+  }
 }
 
 export default Board;
