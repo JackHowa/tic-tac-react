07a2e7105ea289b94a8827b61bb7bfbf0039de23
jackhowa <jackhowaa@gmail.com>
2018-05-27 12:14:18 -0500

    Add outer message showing output props

---
+++ src/Result.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// component is function returning jsx 
+// this is a simple stateless function
+const Result = (props) => <p>{props.outputMessage}</p>;
+
+export default Result;
