a535b7ad1ae9fd4443b818e390f04cad1a59697a
jackhowa <jackhowaa@gmail.com>
2018-05-27 12:14:06 -0500

    Add winner checks and message display

--- src/Board.js
+++ src/Board.js
@@ -1,83 +1,128 @@
-import React, { Component } from 'react';
-import Cell from './Cell.js';
-import CharacterPicker from './CharacterPicker.js';
-import './grid.css';
+import React, { Component } from "react";
+import Cell from "./Cell.js";
+import CharacterPicker from "./CharacterPicker.js";
+import "./grid.css";
+import Result from "./Result";
 
 // constants go outside of the class
 const WINNING_SOLUTIONS = [
   // horizontal
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
   // vertical
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
   // diagonal
-  [0,4,8],
-  [2,4,8]
+  [0, 4, 8],
+  [2, 4, 8]
 ];
 
 class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cells: Array(9).fill('_'),
-      humanCharacter: '',
+      cells: Array(9).fill("_"),
+			humanCharacter: "",
+			outputMessage: ""
     };
   }
 
-  checkWinner = () => {
+  checkWinner = (targetCharacter = "X") => {
     // findIndex returns the first time it matches
     // isWinner checks for match
     // might be implicit true
-    console.log(WINNING_SOLUTIONS.findIndex(solution => this.isWinner(solution, 'X')));
+    // if -1 then no solution found
+    let winnerResult = WINNING_SOLUTIONS.findIndex(solution =>
+      this.isWinner(solution, targetCharacter)
+    );
+    let outputMessage;
+    switch (winnerResult) {
+      case -1:
+        outputMessage = "no winner";
+        break;
+      case 0:
+      case 1:
+      case 2:
+        outputMessage = targetCharacter + " won via horizontal";
+        break;
+      case 3:
+      case 4:
+      case 5:
+        outputMessage = targetCharacter + " won via vertical";
+        break;
+      case 6:
+			case 7:
+
+        outputMessage = targetCharacter + " won via diagonal";
+        break;
+      default:
+        outputMessage = "No winner or unrecognized solution";
+		}
+		// pass down the props of outputMessage
+		// todo: refactor naming convention for game status
+    this.setState({
+			outputMessage
+		})
   };
 
-  isWinner = (targetIndex, humanCharacter = 'X') => {
-    let targetValues = this.state.cells.filter((input, index) => targetIndex.includes(index)); // ["X","X","X"]
+  isWinner = (targetIndex, humanCharacter) => {
+    // find the values at the index
+    let targetValues = this.state.cells.filter((input, index) => {
+      return targetIndex.includes(index);
+    }); // ["X","X","X"]
 
     // will return true if all match
     return targetValues.every(value => value === humanCharacter);
   };
 
-  pickCharacter = (character) => {
+  pickCharacter = character => {
     this.setState({
-      humanCharacter: character,
+      humanCharacter: character
     });
   };
 
   fillCharacter = (humanCharacter, index) => {
-    if (humanCharacter === '') {
-      alert('Please pick a character -- X or 0');
+    if (humanCharacter === "") {
+      alert("Please pick a character -- X or 0");
       return;
     }
     let cells = this.state.cells;
     cells[index] = humanCharacter;
     this.setState({
-      cells,
+      cells
     });
     this.checkWinner();
   };
 
   renderCell(index) {
-    return <Cell index={index} value={this.state.cells[index]}
-                 onClickFunction={this.fillCharacter}
-                 humanCharacter={this.state.humanCharacter}/>;
+    return (
+      <Cell
+        index={index}
+        value={this.state.cells[index]}
+        onClickFunction={this.fillCharacter}
+        humanCharacter={this.state.humanCharacter}
+      />
+    );
   }
 
   render() {
     return (
+      <div>
         <div>
-          <div>
-            <CharacterPicker onClickFunction={this.pickCharacter}
-                             character={'O'}/>
-            <CharacterPicker onClickFunction={this.pickCharacter}
-                             character={'X'}/>
-          </div>
-          <div className={'container'}>
-            <table>
-              <tbody>
+          <CharacterPicker
+            onClickFunction={this.pickCharacter}
+            character={"O"}
+          />
+          <CharacterPicker
+            onClickFunction={this.pickCharacter}
+            character={"X"}
+          />
+        </div>
+        <div className={"container"}>
+          <table>
+            <tbody>
               <tr>
                 {this.renderCell(0)}
                 {this.renderCell(1)}
@@ -93,12 +138,12 @@ class Board extends Component {
                 {this.renderCell(7)}
                 {this.renderCell(8)}
               </tr>
-              </tbody>
-            </table>
-          </div>
+            </tbody>
+					</table>
+					<Result outputMessage={this.state.outputMessage}/>
         </div>
-    )
-        ;
+      </div>
+    );
   }
 }
 
